2075d78716600c3f6ab02ca41465ac4199ddfbed
vicente_leonardo <vicente@geodados.com.br>
2021-07-01 11:05:46 -0300

    Criação das Models Categoria e Livro

---
+++ CRUD_ENTITY_FRAMEWORK_CORE/Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_ENTITY_FRAMEWORK_CORE.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+
+        }
+        public Categoria(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public ICollection <Livro> Livros { get; set; }
+    }
+}
---
+++ CRUD_ENTITY_FRAMEWORK_CORE/Models/Livro.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_ENTITY_FRAMEWORK_CORE.Models
+{
+    public class Livro
+    {
+        public Livro() =>
+          DataEntrada = DateTime.Now;
+        public Livro(int id, int categoriaId, string titulo, string autor, int quantidade, decimal preco, DateTime dataEntrada) : this()
+        {
+            Id = id;
+            CategoriaId = categoriaId;
+            Titulo = titulo;
+            Autor = autor;
+            Quantidade = quantidade;
+            Preco = preco;
+            DataEntrada = dataEntrada;
+        }
+
+        public Livro(int id, int categoriaId, string titulo, string autor, int quantidade, decimal preco) : this()
+        {
+            Id = id;
+            CategoriaId = categoriaId;
+            Titulo = titulo;
+            Autor = autor;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+
+
+
+
+        public int Id { get; set; }
+        public int CategoriaId { get; set; } // Nao preciso desse atributo porque o EF já criaria uma relação só com o atributo Categoria
+        public Categoria Categoria { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataEntrada { get; set; }
+
+
+    }
+}
