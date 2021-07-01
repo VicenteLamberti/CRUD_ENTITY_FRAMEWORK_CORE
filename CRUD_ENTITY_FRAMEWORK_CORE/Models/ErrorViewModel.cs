b6795a896c61fd0ec09e5bdf0f43a5238af57a22
vicente_leonardo <vicente@geodados.com.br>
2021-07-01 10:49:26 -0300

    Add project files.

---
+++ CRUD_ENTITY_FRAMEWORK_CORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_ENTITY_FRAMEWORK_CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
