2a38a99832d8f1c8d88ea12142adfb71e4a7639e
vicente_leonardo <vicente@geodados.com.br>
2021-07-01 12:19:23 -0300

    Injecao de dependência no dbcontext String de conexão no appsettings

--- CRUD_ENTITY_FRAMEWORK_CORE/Context/DataContext.cs
+++ CRUD_ENTITY_FRAMEWORK_CORE/Context/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using CRUD_ENTITY_FRAMEWORK_CORE.Models;
+﻿using CRUD_ENTITY_FRAMEWORK_CORE.Mappings;
+using CRUD_ENTITY_FRAMEWORK_CORE.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -20,9 +21,17 @@ namespace CRUD_ENTITY_FRAMEWORK_CORE.Context
         //        .Property(p => p.Nome)
         //        .HasColumnType("varchar(100)");
 
-        //    Esse método aqui é para mapear, porém se fizer tudo aqui, fica desorganizado. Então é bom criar varias classes
+        //    Esse método aqui é para mapear, porém se fizer tudo aqui, fica desorganizado. Então é bom criar varias classes no Mappings
         //}
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly( // ASSIM BUSCA EM TODO ASSEMBLY A ASSINATURA  IEntityTypeConfiguration<>
+            modelBuilder.ApplyConfiguration(new CategoriaMap()); //PODER SER ASSIM, MAS É PRECISO DIGITAR TODOS
+            modelBuilder.ApplyConfiguration(new LivroMap());
+
+        }
 
 
 
